5b866d245cbda8c24e3240d3a3c04fd6c9fb3612
Robby Bourne <robbybourne1@gmail.com>
2017-08-24 11:15:23 -0400

    Began Robot Model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using robots_dotnet.Models;
 
 namespace robots_dotnet.Controllers
@@ -12,21 +14,17 @@ namespace robots_dotnet.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+             using (var reader = new StreamReader(System.IO.File.Open("robotsData.Json", FileMode.Open)))
+             using (var jsonReader = new JsonTextReader(reader))
+                while (reader.Peek() >= 0)
+                {
+                    var user = reader.ReadToEnd();
+                    var data = user.Split(',');
+                    for (int i = 0; i < data.Length - 5; i += 5);
+                    
+                }
+            return null;
+            
         }
 
         public IActionResult Error()
---
+++ Models/RobotModel.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using robots_dotnet.Models;
+
+namespace robots_dotnet.Models
+{
+    public class RobotController
+    {
+        public int ID { get; set; }
+        public string UserName{ get; set; }
+        public string Name { get; set; }
+        public string Avatar { get; set; }
+        public string Email { get; set; }
+        public string University { get; set; }
+        public string Job { get; set; }
+        public string Company { get; set; }
+        public List<string> Skills { get; set; } = new List<string>();
+        public string Phone { get; set; }
+        public Dictionary<string, string> Address { get; set; } = new Dictionary<string, string>();
+
+        
+    }
+}

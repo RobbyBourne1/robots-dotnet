dadd1b357cc0577d44b45a867498a42349dfb550
Robby Bourne <robbybourne1@gmail.com>
2017-08-24 11:36:48 -0400

    Controller set up

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,18 +13,10 @@ namespace robots_dotnet.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index()
-        {
-             using (var reader = new StreamReader(System.IO.File.Open("robotsData.Json", FileMode.Open)))
-             using (var jsonReader = new JsonTextReader(reader))
-                while (reader.Peek() >= 0)
-                {
-                    var user = reader.ReadToEnd();
-                    var data = user.Split(',');
-                    for (int i = 0; i < data.Length - 5; i += 5);
-                    
-                }
-            return null;
-            
+        {   
+            var bob = new RobotModel();
+            var robotList = bob.Builder();
+            return View(robotList);   
         }
 
         public IActionResult Error()
--- Models/RobotModel.cs
+++ Models/RobotModel.cs
@@ -10,10 +10,10 @@ using robots_dotnet.Models;
 
 namespace robots_dotnet.Models
 {
-    public class RobotController
+    public class RobotModel
     {
         public int ID { get; set; }
-        public string UserName{ get; set; }
+        public string UserName { get; set; }
         public string Name { get; set; }
         public string Avatar { get; set; }
         public string Email { get; set; }
@@ -24,6 +24,23 @@ namespace robots_dotnet.Models
         public string Phone { get; set; }
         public Dictionary<string, string> Address { get; set; } = new Dictionary<string, string>();
 
-        
+    public List<RobotModel> Builder()
+    {
+        JsonSerializer serializer = new JsonSerializer();
+        var robotList = new List<RobotModel>();
+
+           using (var reader = new StreamReader(System.IO.File.Open("robotsData.Json", FileMode.Open)))
+             using (var jsonReader = new JsonTextReader(reader))
+                while (reader.Peek() >= 0)
+                {
+                    var robotReader = JsonConvert.DeserializeObject<RobotModel>(reader.ReadToEnd());
+                    // var data = user.Split(',');
+                    robotList.Add(robotReader);
+                }
+        return robotList;
     }
+
+    }
+
+    
 }
